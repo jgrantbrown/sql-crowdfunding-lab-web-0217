559f6199d3d22e30a6b9b895513c4344748311a9
Your Name <you@example.com>
2017-02-12 08:59:07 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal DECIMAL(10,2),
+start_date TEXT,
+end_date TEXT
+)
+;
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+)
+;
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount DECIMAL(10,2),
+user_id INTEGER,
+project_id INTEGER
+)
+;
